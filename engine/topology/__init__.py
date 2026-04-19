03fae647f57ab8a4e160fff80ac7eabed0a301a3
Stefan <stefankumarasinghe@gmail.com>
2026-04-19 17:01:36 +0530

    (chore) added notice docstrings

--- engine/topology/__init__.py
+++ engine/topology/__init__.py
@@ -3,6 +3,12 @@ Topology analysis package exports.
 
 This package provides dependency-graph primitives and blast-radius helpers used by the RCA pipeline to reason about
 service impact propagation.
+
+Copyright (c) 2026 Stefan Kumarasinghe
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
 """
 
 from engine.topology.graph import BlastRadius, DependencyGraph
